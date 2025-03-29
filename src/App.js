59be1c8bb14992a5c4cc331eaccd3a28e8c600e2
katari-8055 <kataricoder@gmail.com>
2025-03-29 13:33:05 +0530

    i added dark mode and light mode according to need using a toggle switch buttton

--- src/App.js
+++ src/App.js
@@ -5,9 +5,20 @@ import TextForm from './components/TextForm';
 import React, { useState} from 'react'
 function App() {
   const [mode, setMode] = useState('light')
+ 
+  const toggle = () =>{
+    if(mode === 'light' ){
+      setMode('dark')
+      
+    }else{
+      setMode('light')
+      
+    }
+  }
+
   return (
     <div className="App">
-      <Navbar title = "Katari"  mode = {mode} />
+      <Navbar title = "Katari"  mode = {mode} toggle ={toggle}  />
       <div className='container'>
       <TextForm title = "Enter Your Text To Analysis"/>
       </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,9 +6,7 @@ function Navbar(props) {
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
   <div className="container-fluid">
     <a className="navbar-brand" href="#">{props.title}</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
+    
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav">
         <li className="nav-item">
@@ -24,6 +22,10 @@ function Navbar(props) {
           <a className="nav-link disabled" aria-disabled="true">Disabled</a>
         </li>
       </ul>
+    </div>
+    <div class={`form-check form-switch text-${props.mode === 'light'? 'dark': 'light'}`}>
+     <input class="form-check-input" onClick={props.toggle} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+     <label class="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
     </div>
   </div>
 </nav>
