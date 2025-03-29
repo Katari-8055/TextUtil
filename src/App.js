cd0a7cb2bc3fec666e001ad1c375faa084e89d64
katari-8055 <kataricoder@gmail.com>
2025-03-29 13:12:28 +0530

    try to add dark mode and light mode btn

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-
+import React, { useState} from 'react'
 function App() {
+  const [mode, setMode] = useState('light')
   return (
     <div className="App">
-      <Navbar title = "Katari"/>
+      <Navbar title = "Katari"  mode = {mode} />
       <div className='container'>
       <TextForm title = "Enter Your Text To Analysis"/>
       </div>
